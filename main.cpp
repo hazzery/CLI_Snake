ca23ec6e865c1a809e9731fb1f4b3206848fc00c
Harry Parkes <harrydparkes@protonmail.com>
2021-06-16 15:17:53 +1200

    including in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include "include/Coords.h"
+#include "include/Board.h"
+#include "include/Snake.h"
+#include "src/Board.cpp"
 using std::string;
 using std::cout;
 using std::cin;
@@ -6,5 +10,8 @@ using std::endl;
 
 int main()
 {
-    
+    Board<14, 8> board;
+    Snake snake;
+
+    board.print();
 }
