f08ebd1f7d8182c94bc367e489021944a54dac64
Harry Parkes <parkesh@stu.otc.school.nz>
2021-06-18 11:01:55 +1200

    improved readability in board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -7,21 +7,22 @@ using std::endl;
 template <int X, int Y>
 Board<X, Y>::Board()
 {
-    for (int y = 0; y < Y; y++)
+    for (int y = 0; y < height; y++)
     {
-        for (int x = 0; x < X; x++)
+        for (int x = 0; x < width; x++)
         {
             pixelArray[x][y] = '0';
         }
     }
+    cout << "constructed board of dimension " << width << " by " << height << endl;
 }
 
 template <int X, int Y>
 void Board<X, Y>::print()
 {
-    for (int y = 0; y < Y; y++)
+    for (int y = 0; y < height; y++)
     {
-        for (int x = 0; x < X; x++)
+        for (int x = 0; x < width; x++)
         {
             cout << pixelArray[x][y];
         }
@@ -33,4 +34,6 @@ template <int X, int Y>
 void Board<X, Y>::set(Coords pos, unsigned char symbol)
 {
     pixelArray[pos.get(Axis::x)][pos.get(Axis::y)] = symbol;
+
+    cout << "set called: (" << pos.get(Axis::x) << ", " << pos.get(Axis::y) << ") to " << symbol << endl;
 }
