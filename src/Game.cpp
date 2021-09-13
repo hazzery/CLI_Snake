6981d496485d544efbc1e69739f23ca44d52ca4d
Harry Parkes <parkesh@stu.otc.school.nz>
2021-09-14 11:37:28 +1200

    Fixed random indentation in Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -19,60 +19,60 @@ CLI_Snake<X, Y>::CLI_Snake()
 template <int X, int Y>
 bool CLI_Snake<X, Y>::Game()
 {
-        snake.Move(dir, oldDir); //Move the snake in the chosen direction
-        oldDir = dir; //Store the previos movement direction.
+    snake.Move(dir, oldDir); //Move the snake in the chosen direction
+    oldDir = dir; //Store the previos movement direction.
 
-        switch (board.Get(snake.HeadCoords())) //Check what is currently at the co-ordinates the snake just moved to
+    switch (board.Get(snake.HeadCoords())) //Check what is currently at the co-ordinates the snake just moved to
+    {
+    case -1: //Snake hit a wall
+        cout << "That was a wall, try not doing that next time " << endl;
+        return false;
+
+    case 0: //Empty space
+        break;
+
+    case 1: //Snake crashed into itself
+        cout << "Ha! You crashed into yourself" << endl;
+        return false;
+
+    case 2: //Found an apple
+        snake.Eat(apple);
+        break;
+    }
+
+    snake.UpdateBody(); //Update the coordinates stored in the snake body vector
+
+    board.Set(snake.HeadCoords(), snake.HeadSymbol()); //Move snake's head to new pos
+    board.Set(snake.OldHeadCoords(), snake.BodySymbol()); //Show snake's previous head coordinates as body
+    board.Clear(snake.TailCoords()); //Maintain same snake body length
+
+    board.Set(apple.Pos(), apple.Sym()); //show apple
+
+    cout << "\x1B[1J\x1B[H"; //Clear console
+
+    board.Print(); //Display board in console
+
+    if (_kbhit()) //If the keyboard is being pressed
+    {
+        keyboardInput = _getch();
+        switch (keyboardInput)
         {
-        case -1: //Snake hit a wall
-            cout << "That was a wall, try not doing that next time " << endl;
-            return false;
-
-        case 0: //Empty space
+        case 119:// 'w' in ASCII
+            dir = Direction::Up;
             break;
-
-        case 1: //Snake crashed into itself
-            cout << "Ha! You crashed into yourself" << endl;
-            return false;
-
-        case 2: //Found an apple
-            snake.Eat(apple);
+        case 115:// 's'
+            dir = Direction::Down;
+            break;
+        case 97:// 'a'
+            dir = Direction::Left;
+            break;
+        case 100:// 'd'
+            dir = Direction::Right;
+            break;
+        default:
             break;
         }
+    }
 
-        snake.UpdateBody(); //Update the coordinates stored in the snake body vector
-
-        board.Set(snake.HeadCoords(), snake.HeadSymbol()); //Move snake's head to new pos
-        board.Set(snake.OldHeadCoords(), snake.BodySymbol()); //Show snake's previous head coordinates as body
-        board.Clear(snake.TailCoords()); //Maintain same snake body length
-
-        board.Set(apple.Pos(), apple.Sym()); //show apple
-
-        cout << "\x1B[1J\x1B[H"; //Clear console
-
-        board.Print(); //Display board in console
-
-        if (_kbhit()) //If the keyboard is being pressed
-        {
-            keyboardInput = _getch();
-            switch (keyboardInput)
-            {
-            case 119:// 'w' in ASCII
-                dir = Direction::Up;
-                break;
-            case 115:// 's'
-                dir = Direction::Down;
-                break;
-            case 97:// 'a'
-                dir = Direction::Left;
-                break;
-            case 100:// 'd'
-                dir = Direction::Right;
-                break;
-            default:
-                break;
-            }
-        }
-
-        return true;
+    return true;
 }
